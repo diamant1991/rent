8a6a7f7b2e2f5e579eb3af55e6108ffe87f7d289
diamant1991 <diamant1988@mail.ru>
2016-02-06 11:37:18 +0200

    add popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$('input,textarea').focus(function(){
+  $(this).data('placeholder',$(this).attr('placeholder'))
+  $(this).attr('placeholder','');
+});
+$('input,textarea').blur(function(){
+  $(this).attr('placeholder',$(this).data('placeholder'));
+});
+
 $('.slct').click(function(){
 	var dropBlock = $(this).parent().find('.drop');
 
